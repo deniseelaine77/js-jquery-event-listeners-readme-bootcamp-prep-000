f585a1a19b0fcd41a4d5afb051009c74e82003c8
Denise Hastings <dehast59@gmail.com>
2018-09-14 06:57:06 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,9 @@ function frameIt() {
 
 function pressIt() {
   $('form').on('keydown', function(key) {
-    if(key.which ==)
+    if(key.which == 103){
+      alert('G was pressed')
+    }
   }) 
 }
 
