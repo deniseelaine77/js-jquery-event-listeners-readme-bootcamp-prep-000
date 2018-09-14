b61d748ec715d6d1a9f7c21bf9a67b05c16e8c0f
Denise Hastings <dehast59@gmail.com>
2018-09-14 06:41:09 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,6 +2,12 @@
 function getIt() {
   $('p').on('click', function(key) {
   alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img .tasty').on('load', function() {
+    $('img' .tasty').border
   })
 }
 
